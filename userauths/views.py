9764a24678b1c89b9f360cc3acb93144aae455fb
AnnRedCrow <anna.tolokonnikova@outlook.com>
2023-12-19 17:23:05 +0300

    Registrartion

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
-# Create your views here.
+
+def register_view(request):
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Привет, {username}! Регистрация прошла успешно.')
+            login(request, new_user)
+            return redirect('core:home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, "userauths/sign-up.html", context)
